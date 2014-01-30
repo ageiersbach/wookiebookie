9a476c6366453285813058234adab005c6a1d215
Anne Geiersbach <annasazi@gmail.com>
2014-01-30 09:28:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#WookieBookie
+
+An app to track your wookie races!
+
+###Models:
+
+ - wookies
+ - races
+ - bets
+ - users
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wookiebookie::Application.config.secret_token = '1a7779c732a5af5acc879e73af9dc4cea0479fce148ca6c3f526ec6a9d69b14000518664250cc04867ce4ef9bb9b450dcc6efdabe8b103b29e949b8602be6976'
