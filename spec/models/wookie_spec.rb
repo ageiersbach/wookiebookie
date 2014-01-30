c425c8abd20827848afcfee37ef1a80c282f6ea5
Anne Geiersbach <annasazi@gmail.com>
2014-01-30 15:14:56 -0500

    Created wookie_spec test

---
+++ spec/models/wookie_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Wookie do
+  subject { FactoryGirl.create(:wookie) }
+  its(:name) { should == 'Chewwie'}
+  its(:best_time) { should == 15.457}
+  its(:odds) { should == 0.1112 }
+end
