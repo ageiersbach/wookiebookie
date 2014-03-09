0f90a9e3d2ba462c315370ec8f4d0eef92c25215
ageiersbach <annasazi@gmail.com>
2014-03-09 12:37:10 -0400

    Added method to assign random distance to new races.

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,5 +3,11 @@ class Race < ActiveRecord::Base
   has_many :runs
   has_many :wookies, :through => :runs
   validates_presence_of :location
+  before_save :add_distance
 
+  private
+
+  def add_distance
+    self.distance = Random.rand(1..20)
+  end
 end
